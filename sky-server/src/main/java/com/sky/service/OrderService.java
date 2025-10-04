6ab2249ba755085b1a7476ae7074f055e6c7a72e
ZvHivTa <ZvHivTa@gmail.com>
2025-10-04 18:41:57 +0900

    9.28

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,8 +1,26 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 
 public interface OrderService {
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    OrderVO details(Long id);
+
+    void userCancelById(Long id) throws Exception;
+
+    void repetition(Long id);
+
+    void reminder(Long id);
+
+    void paySuccess(String outTradeNo);
 }
